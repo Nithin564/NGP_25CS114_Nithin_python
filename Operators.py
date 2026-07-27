184418fdc7f061de7fd7a522edc9177b8a1adbb9
Nithin564 <25cs114@drngpit.ac.in>
2026-07-27 21:57:28 +0530

    Operators file added

---
+++ Operators.py
@@ -0,0 +1,22 @@
+x = 65
+y = 15
+print(x/y)
+print(x//y)
+
+print(2*3)
+print(2**3)
+
+print(True and False)
+print(True or False)
+
+a = 2
+b = 3
+print(a^b)
+print(a<<2)
+print(a>>2)
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+
