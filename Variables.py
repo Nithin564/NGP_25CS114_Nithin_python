ad7f305f8888ae9a9b04a16e57bada1f57aa42d9
Nithin564 <25cs114@drngpit.ac.in>
2026-07-27 21:31:11 +0530

    Variables file added

---
+++ Variables.py
@@ -0,0 +1,35 @@
+x = 100
+print(x)
+print(type(x))
+
+x = "python"
+print(x)
+print(type(x))
+
+y = 200
+print(y)
+
+x = y = z = 300
+print(x)
+print(y)
+print(z)
+
+rno, stname, rank, mark = "st-1", "abc", 1, 98.96
+print(rno)
+print(stname)
+print(rank)
+print(mark)
+
+empid, empname, salary = "emp-1", "abc",25000.0
+print(empid)
+print(empname)
+print(salary)
+
+first_name = "abc"
+print(first_name)
+
+res = lambda x : x+100
+print(res)
+
+res = list(map(lambda x: x+100, [10,20,30,40,50]))
+print(res)
