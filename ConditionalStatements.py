e876a488530aa784b1d590a624db30fac16bbfa6
Nithin564 <25cs114@drngpit.ac.in>
2026-07-27 22:11:23 +0530

    ConditionalStatements file added

---
+++ ConditionalStatements.py
@@ -0,0 +1,33 @@
+a = 200
+if(a>100):
+    print("a is greater than 100")
+
+a = 300
+b = 200
+if(a>b):
+    print("a is greater than b")
+
+else:
+    print("b is greater than a")
+
+c = 300
+d = 300
+if(a>b):
+    print("a is greater than b")
+
+elif(a<b):
+    print("b is greater than a")
+
+else:
+    print("Both are equal")
+
+
+if((30>20) and (20<30)):
+    print("Hello")
+
+    for x in range(5):
+        if(x==5):
+            print(x)
+
+else: 
+    print("welcome")
